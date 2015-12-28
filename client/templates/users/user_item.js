89742774d67612cc25ad624bb8e9003b69178bf0
Flavio Uemura <flaviou@gmail.com>
2015-12-27 19:46:17 -0500

    User Roles admin

--- client/templates/users/user_item.js
+++ client/templates/users/user_item.js
@@ -2,10 +2,6 @@ Template.userItem.events({
 	'click .js-del-user': function() {
 		Session.set("selectedUser", this._id);
 		$("#userDelete").modal("show");
-	},
-	'click .js-edt-user': function() {
-		Session.set("selectedUser", this._id);
-		$("#userEdit").modal("show");
 	}
 });
   
@@ -15,14 +11,20 @@ Template.userDelete.events({
 		Meteor.call('deleteUser', user_id);
 	}
 })
-
 Template.userEdit.events({
-	'submit form': function(event){
-		event.preventDefault();
-		var user_id = Session.get("selectedUser");
-		var full_name = event.target.fullName.value;
-		Meteor.users.update(user_id, {$set: {'profile.full_name': full_name}}, function(error, result) {
-		}); 
-		$("#userEdit").modal("hide");
-	}
+  'submit form': function(event){
+      event.preventDefault();
+      Meteor.users.update(this._id, {$set: {'profile.full_name': this.doc.profile.full_name}}, function(error, result) {
+      });
+      Router.go('/users');
+  }
+})
+
+Template.userEdit.helpers({
+  roleOptions: function() {
+    return { 
+      'admin': 'Admin',
+      'editor':'Editor'
+    };
+  }
 })
