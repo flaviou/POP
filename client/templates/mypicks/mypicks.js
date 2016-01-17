c8365b484706e71ceb09c2d825287bec9cd3a0cb
Flavio Uemura <flaviou@gmail.com>
2016-01-17 02:52:56 -0500

    Delete and Save Picks

--- client/templates/mypicks/mypicks.js
+++ client/templates/mypicks/mypicks.js
@@ -4,13 +4,23 @@ Template.myPicks.events({
   "click .js-toggle-pick-form": function(event) {
     $("#pick_form").toggle('slow');
   },
+  "click .js-delete": function() {
+    Session.set("selectedPickId", this._id);
+    Session.set("selectedPickName", this.pick_name);
+    $("#pickDelete").modal("show");
+  },
   "submit .js-save-pick-form": function(event) {
     event.preventDefault();
     if (Meteor.user()) {
       $(event.target.pick_name).prop('required', true);
       var name = event.target.pick_name.value;
+      var list = $('.list-right ul li');
+      var players = [];
+      list.each(function(index) {
+        players.push(this.id);
+      });
       if (name) {
-        Meteor.call('insertPick', {'pick_name': name});
+        Meteor.call('insertPick', {'pick_name': name, 'players': players});
         $("#btn-plus").click();
       }
     }
@@ -23,3 +33,16 @@ Template.myPickList.helpers({
     return Picks.find();
   }
 });
+
+Template.pickDelete.helpers({
+  pick_name: function() {
+    return Session.get('selectedPickName');
+  }
+});
+
+Template.pickDelete.events({
+  'click .js-del-confirm': function(){
+    var pickId = Session.get("selectedPickId");
+    Meteor.call('deletePick', pickId);
+  }
+})
--- server/pickMethods.js
+++ server/pickMethods.js
@@ -1,4 +1,13 @@
 Meteor.methods({
+  deletePick: function(pickId) {
+    check(pickId, String);
+    var pick = Picks.findOne({_id: pickId});
+    var loggedInUser = Meteor.userId();
+    if (!loggedInUser || (pick.owner != loggedInUser)) {
+      throw new Meteor.Error(403, "Access Denied");
+    }
+    Picks.remove({_id: pickId});
+  },
   insertPick: function(newDoc) {
     newDoc['createdOn'] = new Date();
     newDoc['owner'] = Meteor.userId();
